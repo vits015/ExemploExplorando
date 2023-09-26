f2e99ea13b53388159e8ef9a54879bd204bf3b1b
Vitor Bittencourt <vitorleite015@gmail.com>
2023-09-25 22:56:02 -0300

    adicionada classe Pessoa

---
+++ Models/Pessoa.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploExplorando.Models
+{
+    public class Pessoa
+    {
+        private string _nome,_sobrenome;        
+        private int _idade;
+        public string Nome 
+        { 
+            get => _nome.ToUpper(); 
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O Nome não pode ser em branco");
+                }
+                _nome = value;
+            }
+        }
+        public string Sobrenome 
+        {
+            get => _sobrenome.ToUpper(); 
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O Sobrenome não pode ser em branco");
+                }
+                _sobrenome = value;
+            }
+        }
+        public int Idade 
+        { 
+            get => _idade; 
+            set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("A idade não pode ser menor que 0");
+                }
+                _idade = value;
+            }
+        }
+        public string NomeCompleto { get => $"{Nome} {Sobrenome}".ToUpper(); }
+        
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {NomeCompleto}, Idade: {Idade}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemploExplorando.Models;
+
+Pessoa p = new();
+
+p.Nome = "José";
+p.Sobrenome = "Silva";
+p.Apresentar();
+
+
+
+
